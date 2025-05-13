19ec9a92c442c9881f7f371dc89ad2e8ff7feb65
roohiiitt89 <rohitsb567@proton.me>
2025-05-14 00:13:40 +0530

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,27 +1,39 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
-const NotFound = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.error(
-      "404 Error: User attempted to access non-existent route:",
-      location.pathname
-    );
-  }, [location.pathname]);
-
+export function NotFoundPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
-        </a>
-      </div>
-    </div>
-  );
-};
+    <section className="bg-white font-serif min-h-screen flex items-center justify-center">
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <div className="w-full sm:w-10/12 md:w-8/12 text-center">
+            <div
+              className="bg-[url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif)] h-[250px] sm:h-[350px] md:h-[400px] bg-center bg-no-repeat bg-contain"
+              aria-hidden="true"
+            >
+              <h1 className="text-center text-black text-6xl sm:text-7xl md:text-8xl pt-6 sm:pt-8">
+                404
+              </h1>
+            </div>
 
-export default NotFound;
+            <div className="mt-[-50px]">
+              <h3 className="text-2xl text-black sm:text-3xl font-bold mb-4">
+                Look like you're lost
+              </h3>
+              <p className="mb-6 text-black sm:mb-5">
+                The page you are looking for is not available!
+              </p>
+
+              <Button
+                variant="default"
+                onClick={() => router.push("/")}
+                className="my-5 bg-green-600 hover:bg-green-700 "
+              >
+                Go to Home
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
